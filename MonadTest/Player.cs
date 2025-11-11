c42c6014f0cd02bc9bf260abd089c31471845ba8
Allysh0w <allyson.martinz@gmail.com>
2025-11-11 20:32:50 +0100

    created player class

---
+++ MonadTest/Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace MonadTest
+{
+    public class Player
+    {
+        private readonly int currentAttack;
+        private readonly int defense;
+        private readonly int specialDefense;
+        private readonly int modifier;
+        private readonly int bonus;
+
+        public Player(int currentAttack, int defense, int specialDefense, int modifier, int bonus)
+        {
+            this.currentAttack = currentAttack;
+            this.defense = defense;
+            this.specialDefense = specialDefense;
+            this.modifier = modifier;
+            this.bonus = bonus;
+        }
+
+        public int CurrentAttack => currentAttack;
+        public int Defense => defense;
+        public int SpecialDefense => specialDefense;
+        public int Modifier => modifier;
+        public int Bonus => bonus;
+
+
+        // only examples
+        public static IO<int> calcDamage(int attack, int modifier)
+        {
+            return IO<int>.Of(() => attack * modifier);
+        }
+
+        public static IO<int> calcHitReduced(int playerAttack, int targetDefense)
+        {
+            return IO<int>.Of(() => playerAttack / targetDefense);
+        }
+
+        public static IO<int> generateAttackBonus(int hit, int bonus)
+        {
+            return IO<int>.Of(() => hit * bonus);
+        }
+
+        public static Option<int> triggerTargetSpecialDefense(int attack,
+                                                               int sDefense,
+                                                               int modifierSpecial)
+        {
+            var damage = attack / (sDefense * modifierSpecial);
+            return damage > 0 ? Option<int>.Some(damage) : Option<int>.None;
+        }
+    }
+}
+
--- MonadTest/Program.cs
+++ MonadTest/Program.cs
@@ -9,19 +9,24 @@ namespace MonadTest
         public static void Main(string[] args) 
         {
 
-          //basic example character hit
-            const int player1CurrentAttack = 10;
-            const int player2Defense = 10;
-            const int player2SpecialDefense = 3;
-            const int modifier = 2;
-            const int playerBonus = 3;
+            var player1 = new Player(currentAttack: 10,
+                                     defense: 1,
+                                     specialDefense: 1,
+                                     modifier: 2,
+                                     bonus: 3);
 
-           // must be const IO<Option<int>>
-            var attackResult = IO<int>.Of(() => player1CurrentAttack)
-            .FlatMap(attack => calcDamage(attack, modifier))
-            .FlatMap(res => calcHitReduced(res, player2Defense))
-            .FlatMap(hitReduced => generateAttackBonus(hitReduced, playerBonus))
-            .Map(finalHit => triggerTargetSpecialDefense(finalHit, player2SpecialDefense, 3));
+            var player2 = new Player(currentAttack: 10,
+                                     defense: 10,
+                                     specialDefense: 3,
+                                     modifier: 2,
+                                     bonus: 3);
+
+            // must be const IO<Option<int>>
+            var attackResult = IO<int>.Of(() => player1.CurrentAttack)
+            .FlatMap(attack => Player.calcDamage(attack, player2.Modifier))
+            .FlatMap(res => Player.calcHitReduced(res, player2.Defense))
+            .FlatMap(hitReduced => Player.generateAttackBonus(hitReduced, player1.Bonus))
+            .Map(finalHit => Player.triggerTargetSpecialDefense(finalHit, player2.SpecialDefense, player2.Bonus));
 
             // must be const
             var renderDamage = attackResult.Map(x => x.Match(
@@ -29,32 +34,9 @@ namespace MonadTest
                 none: () => "MISS")
             );
 
-            Console.WriteLine(renderDamage.Pure()); // Result? 
+            Console.WriteLine(renderDamage.Pure());
 
 
-        }
-
-        private static IO<int> calcDamage(int attack, int modifier)
-        {
-            return IO<int>.Of(() => attack * modifier);
-        }
-
-        private static IO<int> calcHitReduced(int playerAttack, int targetDefense)
-        {
-            return IO<int>.Of(() => playerAttack / targetDefense);
-        }
-
-        private static IO<int> generateAttackBonus(int hit, int bonus)
-        {
-            return IO<int>.Of(() => hit * bonus); 
-        }
-
-        private static Option<int> triggerTargetSpecialDefense(int attack, 
-                                                               int sDefense, 
-                                                               int modifierSpecial)
-        {
-            var damage = attack / (sDefense * modifierSpecial); 
-            return damage > 0 ? Option<int>.Some(damage) : Option<int>.None; // just test
         }
 
     }
